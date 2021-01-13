d846821d77359c4d87d1d4168b9640456360c6b7
Ravil Latypov <ravillatypov12@gmail.com>
2021-01-13 15:39:05 +0300

    fix

--- app/api/request/calls/get_calls.py
+++ app/api/request/calls/get_calls.py
@@ -8,7 +8,7 @@ from app.consts import CallState, CallType
 class RequestGetCalls(BaseModel):
     state: CallState = Field(None, parameter='query')
     need_recall: bool = Field(None, parameter='query')
-    started_from: datetime = Field(default_factory=lambda _: datetime.utcnow() - timedelta(days=30), parameter='query')
+    started_from: datetime = Field(default_factory=lambda: datetime.utcnow() - timedelta(days=30), parameter='query')
     started_to: datetime = Field(None, parameter='query')
     call_type: CallType = Field(None, parameter='query')
     limit: int = Field(10000, parameter='query')
